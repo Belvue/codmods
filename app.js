5dd2359759684a3dd723d50627ad261f152e823b
SwekAap <snipaahcj@gmail.com>
2018-10-10 15:07:07 +0200

    initial commit :monkey_face:

---
+++ app.js
@@ -0,0 +1,111 @@
+﻿'use strict';
+var debug = require('debug');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var passport = require('passport');
+var session = require('express-session');
+var SteamStrategy = require('passport-steam').Strategy;
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new SteamStrategy({
+    returnURL: 'http://localhost:8080/login/return',
+    realm: 'http://localhost:8080/',
+    apiKey: '7BABB0D2D8C00AE40D6D4201CAB5D14F'
+},
+    function (identifier, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // To keep the example simple, the user's Steam profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Steam account with a user record in your database,
+            // and return that user instead.
+            profile.identifier = identifier;
+            return done(null, profile);
+        });
+    }
+));
+
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(session({
+    secret: 'swekkiesessie123',
+    name: 'swekkiesessie',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(8080, function () {
+    debug('Express server listening on port ' + server.address().port);
+});
---
+++ routes/index.js
@@ -0,0 +1,67 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var connection = require('../public/javascripts/db')
+
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index.html', { title: 'Mods & Maps', req: req });
+});
+
+router.get('/maps', function (req, res) {
+    res.render('maps.html', { title: 'Maps', req: req });
+});
+
+router.get('/add-mod', function (req, res) {
+    res.render('mod_add.html', { title: 'Add mod', req: req });
+});
+
+router.post('/add-mod', function (req, res) {
+
+    connection.query(' INSERT INTO mods(type, name, author, url, status) VALUES("' + req.body.type + '","' + req.body.mod_name + '", "' + req.user._json.steamid + '","' + req.body.mod_link + '", 0)', function (err, rows, fields) {
+        if (err) throw err
+        res.send('The map is: ' + req.body.mod_name);
+    });
+
+});
+
+router.get('/pending-mods', function (req, res) {
+
+    connection.query('SELECT * FROM mods WHERE status = 0', function (err, rows, fields) {
+        //if (err) res.send(err)
+        res.render('mod_pending.html', { title: 'Pending mods', req: req, rows: rows });
+    });
+    
+});
+
+router.get('/approve-mod/:id', function (req, res) {
+    connection.query('UPDATE mods SET status = 1 WHERE id = ' + req.params.id, function (err, rows, fields) {
+        res.redirect('/pending-mods');
+    });
+});
+
+router.get('/dismiss-mod/:id', function (req, res) {
+    connection.query('UPDATE mods SET status = 2 WHERE id = ' + req.params.id, function (err, rows, fields) {
+        res.redirect('/pending-mods');
+    });
+});
+
+router.get('/login',
+    passport.authenticate('steam', { failureRedirect: '/' }),
+    function (req, res) {
+        res.redirect('/');
+    });
+
+router.get('/login/return',
+    function (req, res, next) {
+        req.url = req.originalUrl;
+        next();
+    },
+    passport.authenticate('steam', { failureRedirect: '/' }),
+    function (req, res) {
+        res.redirect('/');
+    });
+
+module.exports = router;
